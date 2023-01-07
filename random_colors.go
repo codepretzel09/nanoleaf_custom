78ee81dbe915b0621be19de941be8985142d64a9
codepretzel09 <68131212+codepretzel09@users.noreply.github.com>
2023-01-07 12:03:02 -0700

    one

--- random_colors.go
+++ random_colors.go
@@ -12,14 +12,18 @@ import (
 )
 
 func main() {
+	// Seed the random number generator with the current Unix time in nanoseconds
 	rand.Seed(time.Now().UnixNano())
+	// Get the panel layout and set the colors
 	getpanels()
 }
 
+// Generate a random color value between 0 and 255 (inclusive)
 func generateRandomColorValue() int {
 	return rand.Intn(256)
 }
 
+// PositionData represents the data for a panel's position
 type PositionData struct {
 	PanelID int `json:"panelId"`
 	X       int `json:"x"`
@@ -28,35 +32,45 @@ type PositionData struct {
 	Shape   int `json:"shapeType"`
 }
 
+// Response represents the data returned in the response to a panel layout request
 type Response struct {
 	NumPanels    int            `json:"numPanels"`
 	SideLength   int            `json:"sideLength"`
 	PositionData []PositionData `json:"positionData"`
 }
 
+// getpanels gets the panel layout and sets the colors for each panel
 func getpanels() {
-	url := "http://192.168.1.97:16021/api/v1/BStmnw5eZu5SHZtkTWJwYpUfSV538TH2/panelLayout/layout"
+	// URL for the panel layout endpoint
+	url := "http://192.168.x.x:16021/api/v1/AUTHTOKEN/panelLayout/layout"
+	// HTTP method for the request
 	method := "GET"
+	// Create a new HTTP client
 	client := &http.Client{}
+	// Create a new HTTP request with the specified method and URL
 	req, err := http.NewRequest(method, url, nil)
 
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+	// Send the request and get the response
 	res, err := client.Do(req)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+	// Close the response body when the function returns
 	defer res.Body.Close()
 
+	// Read the response body
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
+	// Unmarshal the response body into a Response struct
 	var response Response
 	err = json.Unmarshal(body, &response)
 	if err != nil {
@@ -64,7 +78,9 @@ func getpanels() {
 		return
 	}
 
+	// Iterate over each panel in the PositionData slice
 	for _, panel := range response.PositionData {
+		// Convert the PanelID to a string
 		panelIDStr := strconv.Itoa(panel.PanelID)
 
 		// Generate a random color
@@ -81,7 +97,7 @@ func getpanels() {
 		fmt.Println(redStr)
 		fmt.Println(greenStr)
 		fmt.Println(blueStr)
-		makeHTTPCall("http://192.168.1.97:16021/api/v1/BStmnw5eZu5SHZtkTWJwYpUfSV538TH2/effects", "PUT", `{"write":{"animData":"1 `+panelIDStr+` 1 `+redStr+` `+greenStr+` `+blueStr+` 0 1","animType":"static","command":"display","loop":false,"palette":[],"version":"1.0"}}`)
+		makeHTTPCall("http://192.168.x.x:16021/api/v1/AUTHTOKEN/effects", "PUT", `{"write":{"animData":"1 `+panelIDStr+` 1 `+redStr+` `+greenStr+` `+blueStr+` 0 1","animType":"static","command":"display","loop":false,"palette":[],"version":"1.0"}}`)
 	}
 
 }
